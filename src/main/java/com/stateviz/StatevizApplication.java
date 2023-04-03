4252f392bdda21262f2a81decd85f3666f24a9cb
Костенко Петр <picalines0@gmail.com>
2023-04-04 02:25:02 +0300

    initial commit

---
+++ src/main/java/com/stateviz/StatevizApplication.java
@@ -0,0 +1,11 @@
+package com.stateviz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatevizApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StatevizApplication.class, args);
+	}
+}
---
+++ src/test/java/com/stateviz/StatevizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stateviz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatevizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
