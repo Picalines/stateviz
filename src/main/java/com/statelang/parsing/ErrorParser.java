a91bfae3d5793ca990e15758682f56b336f986b0
Picalines <picalines0@gmail.com>
2023-04-21 22:59:55 +0300

    ErrorParser

---
+++ src/main/java/com/statelang/parsing/ErrorParser.java
@@ -0,0 +1,31 @@
+package com.statelang.parsing;
+
+import java.util.HashMap;
+
+import com.statelang.diagnostics.Report;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+final class ErrorParser<T> extends Parser<T> {
+
+    private static final HashMap<Report.Kind, ErrorParser<?>> instances = new HashMap<>();
+
+    private final Report.Kind reportKind;
+
+    public static <T> ErrorParser<T> of(Report.Kind reportKind) {
+        @SuppressWarnings("unchecked")
+        var parser = (ErrorParser<T>) instances.computeIfAbsent(reportKind, ErrorParser::new);
+        return parser;
+    }
+
+    @Override
+    public ParserResult<T> parse(ParserContext context) {
+        var reader = context.reader();
+
+        return ParserResult.fromError(Report.builder()
+                .selection(reader.selection())
+                .kind(reportKind));
+    }
+}
--- src/main/java/com/statelang/parsing/Parse.java
+++ src/main/java/com/statelang/parsing/Parse.java
@@ -2,20 +2,12 @@ package com.statelang.parsing;
 
 import java.util.function.Supplier;
 
+import com.statelang.diagnostics.Report;
 import com.statelang.tokenization.Token;
 import com.statelang.tokenization.TokenKind;
 
 public final class Parse {
     private Parse() {
-    }
-
-    public static <T> Parser<T> success(T value) {
-        return new Parser<>() {
-            @Override
-            public ParserResult<T> parse(ParserContext context) {
-                return ParserResult.fromValue(value);
-            }
-        };
     }
 
     public static <T> Parser<T> success(Supplier<T> valueSupplier) {
@@ -25,6 +17,14 @@ public final class Parse {
                 return ParserResult.fromValue(valueSupplier.get());
             }
         };
+    }
+
+    public static <T> Parser<T> success(T value) {
+        return success(() -> value);
+    }
+
+    public static <T> Parser<T> error(Report.Kind reportKind) {
+        return ErrorParser.of(reportKind);
     }
 
     public static Parser<Token> token(TokenKind tokenKind) {
