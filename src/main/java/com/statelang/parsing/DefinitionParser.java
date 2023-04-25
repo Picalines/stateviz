e51b96468e8370a4368037b70c85ea16c64e0760
Костенко Петр <picalines0@gmail.com>
2023-04-25 14:57:51 +0300

    DefinitionParser

---
+++ src/main/java/com/statelang/parsing/DefinitionParser.java
@@ -0,0 +1,44 @@
+package com.statelang.parsing;
+
+import com.statelang.ast.ConstantDefinition;
+import com.statelang.ast.StateDefinition;
+import com.statelang.ast.VariableDefinition;
+import com.statelang.parsing.lib.Parse;
+import com.statelang.parsing.lib.Parser;
+import com.statelang.tokenization.Token;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import static com.statelang.tokenization.Token.Kind.*;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class DefinitionParser {
+
+    public static final Parser<ConstantDefinition> constant = Parse.token(KEYWORD_CONST)
+        .then(Parse.token(IDENTIFIER))
+        .then(
+            nameToken -> Parse.token(OPERATOR_ASSIGN)
+                .then(ValueExpressionParser.lambda)
+                .followedBy(Parse.token(SEMICOLON).recover(() -> null))
+                .map(initialValue -> new ConstantDefinition(nameToken.value(), initialValue))
+        );
+
+    public static final Parser<VariableDefinition> variable = Parse.token(KEYWORD_LET)
+        .then(Parse.token(IDENTIFIER))
+        .then(
+            nameToken -> Parse.token(OPERATOR_ASSIGN)
+                .then(ValueExpressionParser.lambda)
+                .followedBy(Parse.token(SEMICOLON).recover(() -> null))
+                .map(initialValue -> new VariableDefinition(nameToken.value(), initialValue))
+        );
+
+    public static final Parser<StateDefinition> state = Parse.token(KEYWORD_STATE)
+        .then(
+            Parse.token(IDENTIFIER)
+                .map(Token::text)
+                .manyWithDelimiter(Parse.token(COMMA))
+                .map(StateDefinition::new)
+                .between(Parse.token(OPEN_CURLY_BRACE), Parse.token(CLOSE_CURLY_BRACE))
+        );
+}
---
+++ src/test/java/com/statelang/parsing/DefinitionParserTests.java
@@ -0,0 +1,160 @@
+package com.statelang.parsing;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static com.statelang.parsing.lib.ParsingTestUtils.assertParsesWithErrors;
+import static com.statelang.parsing.lib.ParsingTestUtils.assertParsesWithoutErrors;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.statelang.ast.ConstantDefinition;
+import com.statelang.ast.InvalidValueNode;
+import com.statelang.ast.NumberLiteralValue;
+import com.statelang.ast.StateDefinition;
+import com.statelang.ast.VariableDefinition;
+import com.statelang.tokenization.SourceSelection;
+import com.statelang.tokenization.SourceText;
+import com.statelang.tokenization.Token;
+
+class DefinitionParserTests {
+
+    @Nested
+    class Constant {
+
+        @Test
+        void normal() {
+            assertThat(assertParsesWithoutErrors("const x := 123;", DefinitionParser.constant))
+                .usingRecursiveComparison()
+                .ignoringFieldsOfTypes(SourceSelection.class, SourceText.class)
+                .isEqualTo(
+                    new ConstantDefinition(
+                        new Token(null, Token.Kind.IDENTIFIER, "x"),
+                        new NumberLiteralValue(new Token(null, Token.Kind.LITERAL_NUMBER, "123"), 123)
+                    )
+                );
+        }
+
+        @Test
+        void withoutExpression() {
+            assertThat(assertParsesWithErrors("const x := ;", DefinitionParser.constant))
+                .usingRecursiveComparison()
+                .ignoringFieldsOfTypes(SourceSelection.class, SourceText.class)
+                .isEqualTo(
+                    Optional.of(
+                        new ConstantDefinition(
+                            new Token(null, Token.Kind.IDENTIFIER, "x"),
+                            InvalidValueNode.instance
+                        )
+                    )
+                );
+        }
+
+        @Test
+        void withoutSemicollon() {
+            assertThat(assertParsesWithErrors("const x := 123", DefinitionParser.constant))
+                .usingRecursiveComparison()
+                .ignoringFieldsOfTypes(SourceSelection.class, SourceText.class)
+                .isEqualTo(
+                    Optional.of(
+                        new ConstantDefinition(
+                            new Token(null, Token.Kind.IDENTIFIER, "x"),
+                            new NumberLiteralValue(new Token(null, Token.Kind.LITERAL_NUMBER, "123"), 123)
+                        )
+                    )
+                );
+        }
+    }
+
+    @Nested
+    class Variable {
+
+        @Test
+        void normal() {
+            assertThat(assertParsesWithoutErrors("let x := 123;", DefinitionParser.variable))
+                .usingRecursiveComparison()
+                .ignoringFieldsOfTypes(SourceSelection.class, SourceText.class)
+                .isEqualTo(
+                    new VariableDefinition(
+                        new Token(null, Token.Kind.IDENTIFIER, "x"),
+                        new NumberLiteralValue(new Token(null, Token.Kind.LITERAL_NUMBER, "123"), 123)
+                    )
+                );
+        }
+
+        @Test
+        void withoutExpression() {
+            assertThat(assertParsesWithErrors("let x := ;", DefinitionParser.variable))
+                .usingRecursiveComparison()
+                .ignoringFieldsOfTypes(SourceSelection.class, SourceText.class)
+                .isEqualTo(
+                    Optional.of(
+                        new VariableDefinition(
+                            new Token(null, Token.Kind.IDENTIFIER, "x"),
+                            InvalidValueNode.instance
+                        )
+                    )
+                );
+        }
+
+        @Test
+        void withoutSemicollon() {
+            assertThat(assertParsesWithErrors("let x := 123", DefinitionParser.variable))
+                .usingRecursiveComparison()
+                .ignoringFieldsOfTypes(SourceSelection.class, SourceText.class)
+                .isEqualTo(
+                    Optional.of(
+                        new VariableDefinition(
+                            new Token(null, Token.Kind.IDENTIFIER, "x"),
+                            new NumberLiteralValue(new Token(null, Token.Kind.LITERAL_NUMBER, "123"), 123)
+                        )
+                    )
+                );
+        }
+    }
+
+    @Nested
+    class State {
+
+        @Test
+        void emptyList() {
+            assertThat(assertParsesWithoutErrors("state {}", DefinitionParser.state))
+                .usingRecursiveComparison()
+                .isEqualTo(
+                    new StateDefinition(Arrays.asList())
+                );
+        }
+
+        @Test
+        void singleState() {
+            assertThat(assertParsesWithoutErrors("state { SINGLE }", DefinitionParser.state))
+                .usingRecursiveComparison()
+                .isEqualTo(
+                    new StateDefinition(Arrays.asList("SINGLE"))
+                );
+        }
+
+        @Test
+        void manyStates() {
+            assertThat(assertParsesWithoutErrors("state { A, B }", DefinitionParser.state))
+                .usingRecursiveComparison()
+                .isEqualTo(
+                    new StateDefinition(Arrays.asList("A", "B"))
+                );
+
+            assertThat(assertParsesWithoutErrors("state { A, B, C }", DefinitionParser.state))
+                .usingRecursiveComparison()
+                .isEqualTo(
+                    new StateDefinition(Arrays.asList("A", "B", "C"))
+                );
+
+            assertThat(assertParsesWithoutErrors("state { A, B, C, }", DefinitionParser.state))
+                .usingRecursiveComparison()
+                .isEqualTo(
+                    new StateDefinition(Arrays.asList("A", "B", "C"))
+                );
+        }
+    }
+}
