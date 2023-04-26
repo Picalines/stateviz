3421da2b9dc01a415b510d5a0e0aa3b2fbc4d5be
Picalines <picalines0@gmail.com>
2023-04-26 13:30:07 +0300

    counter program test

--- src/test/java/com/statelang/parsing/ProgramParserTests.java
+++ src/test/java/com/statelang/parsing/ProgramParserTests.java
@@ -1,14 +1,23 @@
 package com.statelang.parsing;
 
 import static com.statelang.parsing.lib.ParsingTestUtils.assertParsesWithoutErrors;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
+
+import com.statelang.ast.*;
+import com.statelang.model.BinaryOperator;
+import com.statelang.tokenization.SourceSelection;
+import com.statelang.tokenization.Token;
+import static com.statelang.tokenization.Token.Kind.*;
 
 class ProgramParserTests {
 
     @Test
     void counter() {
-    assertParsesWithoutErrors("""
+        var program = assertParsesWithoutErrors("""
             state {
                 COUNTING,
                 STOPPED,
@@ -27,5 +36,59 @@ class ProgramParserTests {
                 assert count <= max;
             }
             """, ProgramParser.program);
+
+        assertThat(program)
+            .usingRecursiveComparison()
+            .ignoringFieldsOfTypes(SourceSelection.class)
+            .isEqualTo(
+                new Program(
+                    Arrays.asList(
+                        new StateDefinition(Arrays.asList("COUNTING", "STOPPED")),
+                        new ConstantDefinition(
+                            new Token(null, IDENTIFIER, "max"),
+                            new NumberLiteralValue(new Token(null, LITERAL_NUMBER, "10"), 10)
+                        ),
+                        new VariableDefinition(
+                            new Token(null, IDENTIFIER, "count"),
+                            new NumberLiteralValue(new Token(null, LITERAL_NUMBER, "0"), 0)
+                        ),
+                        new InStateDefinition(
+                            new Token(null, IDENTIFIER, "COUNTING"),
+                            new StateActionBlock(
+                                Arrays.asList(
+                                    new AssignmentAction(
+                                        new Token(null, IDENTIFIER, "count"),
+                                        new BinaryValueExpressionNode(
+                                            BinaryOperator.PLUS,
+                                            new VariableValueNode(new Token(null, IDENTIFIER, "count")),
+                                            new NumberLiteralValue(new Token(null, LITERAL_NUMBER, "1"), 1)
+                                        )
+                                    ),
+                                    new ConditionalAction(
+                                        new BinaryValueExpressionNode(
+                                            BinaryOperator.EQUALS,
+                                            new VariableValueNode(new Token(null, IDENTIFIER, "count")),
+                                            new VariableValueNode(new Token(null, IDENTIFIER, "max"))
+                                        ),
+                                        new StateActionBlock(
+                                            Arrays.asList(
+                                                new TransitionAction(new Token(null, IDENTIFIER, "STOPPED"))
+                                            )
+                                        ),
+                                        null
+                                    ),
+                                    new AssertionAction(
+                                        new BinaryValueExpressionNode(
+                                            BinaryOperator.LESS_OR_EQUAL,
+                                            new VariableValueNode(new Token(null, IDENTIFIER, "count")),
+                                            new VariableValueNode(new Token(null, IDENTIFIER, "max"))
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
