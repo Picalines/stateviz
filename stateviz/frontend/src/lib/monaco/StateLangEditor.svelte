a8e8501b1ffb353eaded3ea84d579cbac103c747
Picalines <picalines0@gmail.com>
2023-05-04 20:49:37 +0300

    statelang highlighting

--- stateviz/frontend/src/lib/monaco/StateLangEditor.svelte
+++ stateviz/frontend/src/lib/monaco/StateLangEditor.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import type * as monaco from 'monaco-editor';
 	import MonacoEditor from './MonacoEditor.svelte';
+	import { statelangId } from './statelang';
 
     export let value = '';
 
 	const options: monaco.editor.IStandaloneEditorConstructionOptions = {
         value,
+		language: statelangId,
 		automaticLayout: true,
 		minimap: { enabled: false },
 	};
---
+++ stateviz/frontend/src/lib/monaco/statelang.ts
@@ -0,0 +1,83 @@
+import * as monaco from 'monaco-editor';
+
+export const statelangId = 'statelang';
+
+monaco.languages.register({ id: statelangId });
+
+monaco.languages.setMonarchTokensProvider(statelangId, {
+	keywords: ['state', 'let', 'const', 'when', 'assert', 'if', 'else'],
+
+	operators: [':=', '!=', '=', '+', '-', '*', '/', '%'],
+
+	operatorSymbols: /[=>+\-*\/%:]+/,
+
+	tokenizer: {
+		root: [
+			// identifiers and keywords
+			[
+				/[a-zA-Z_][a-zA-Z0-9_]*/,
+				{
+					cases: {
+						'@keywords': 'keyword',
+						'@default': 'variable.parameter',
+					},
+				},
+			],
+
+			// whitespace
+			{ include: '@whitespace' },
+
+			// strings
+			[/"([^"\\]|\\.)*$/, 'string.invalid'], // non-teminated string
+			[/"/, { token: 'string.quote', bracket: '@open', next: '@string' }],
+
+			// delimiters and operators
+			[/[{}()]/, '@brackets'],
+			[/[<>](?!@operatorSymbols)/, '@brackets'],
+			[
+				/@operatorSymbols/,
+				{
+					cases: {
+						'@operators': 'operator',
+						'@default': '',
+					},
+				},
+			],
+
+			// numbers
+			[/(\d+(\.\d+)?|\.\d+)(\w*)/, 'number.float'],
+
+			// delimiter: after number because of .\d floats
+			[/[;,.]/, 'delimiter'],
+		],
+
+		comment: [[/#.*$/, 'comment']],
+
+		string: [
+			[/[^\\"]+/, 'string'],
+			[/\\./, 'string.escape.invalid'],
+			[/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }],
+		],
+
+		whitespace: [
+			[/[ \t\r\n]+/, 'white'],
+			[/#.*$/, 'comment'],
+		],
+	},
+});
+
+monaco.languages.setLanguageConfiguration(statelangId, {
+	comments: {
+		lineComment: '#',
+	},
+
+	autoClosingPairs: [
+		{ open: '(', close: ')' },
+		{ open: '{', close: '}' },
+	],
+
+	brackets: [
+		['(', ')'],
+		['{', '}'],
+	],
+});
