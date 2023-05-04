f3d426b06f75dbab03da6b05b5fedec50e049d4d
Picalines <picalines0@gmail.com>
2023-05-04 18:16:57 +0300

    svelte frontend

---
+++ stateviz/src/main/java/com/stateviz/controllers/FrontendController.java
@@ -0,0 +1,13 @@
+package com.stateviz.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+
+    @GetMapping("/")
+    public String getIndex() {
+        return "index.html";
+    }
+}
