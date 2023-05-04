15db6d29d6127bd2bbd9fd899cec52599c18d34f
Picalines <picalines0@gmail.com>
2023-05-04 20:04:14 +0300

    basic monaco editor

---
+++ stateviz/frontend/src/lib/StateLangEditor.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type * as monaco from 'monaco-editor';
+	import MonacoEditor from './MonacoEditor.svelte';
+
+    export let value = '';
+
+	const options: monaco.editor.IStandaloneEditorConstructionOptions = {
+        value,
+		automaticLayout: true,
+		minimap: { enabled: false },
+	};
+</script>
+
+<MonacoEditor {options} style="height: 100vh" />
